30c140d434b5010d59df5a7135b8f652b1a17999
CHARAN_CODES <sunkarasreecharan@gmail.com>
2026-08-21 12:40:35 +0530

    Add files via upload

---
+++ Project Files/test/import.py
@@ -0,0 +1,9 @@
+import flask
+import sklearn
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pickle
+
+print("All libraries imported successfully!")
